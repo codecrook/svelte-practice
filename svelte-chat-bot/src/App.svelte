08846d9d0e3332be26b823d9c19ed95a5545da61
Deeptiman Mallick <hello@codecrook.co>
2020-06-06 15:05:47 +0530

    Initial Commit

---
+++ svelte-chat-bot/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+  let name = "Deeptiman";
+  let age = 23;
+  const incrementAge = () => (age += 1);
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <h1>Hello, I'm {name}! I'm {age} years old.</h1>
+  <input type="text" bind:value={name} />
+  <button on:click={incrementAge}>Increase Age</button>
+</main>
+<ContactCard {name} proffession="Frontend Developer" desc="Figuring Out!!" />
---
+++ svelte-chat-bot/src/ContactCard.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name, proffession, desc;
+</script>
+
+<style>
+  .card-container {
+    border: 1px solid black;
+    margin: 25px;
+    padding: 15px;
+  }
+</style>
+
+<div class="card-container">
+  <h2>{name}</h2>
+  <h3>{proffession}</h3>
+  <hr />
+  <p>{desc}</p>
+</div>
