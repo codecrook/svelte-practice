a284fa6518fa2e6ebb79d61e624365da5274360e
Deeptiman Mallick <hello@codecrook.co>
2020-06-28 00:48:03 +0530

    Added Container component and learned about slots

--- svelte-chat-bot/src/App.svelte
+++ svelte-chat-bot/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Face from "./Face.svelte";
+  import Container from "./Container.svelte";
   let say = "hi";
   setTimeout(() => (say = "bye"), 1000);
 </script>
@@ -8,7 +9,19 @@
   div {
     color: red;
   }
+  :global(*) {
+    box-sizing: border-box;
+  }
+  :global(body, html) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
 </style>
 
-<div>{say}</div>
-<Face size={4} />
+<Container>
+  <div>{say}</div>
+  <Face size={4} index={2} />
+  <Face size={3} index={1} />
+  <Face />
+</Container>
--- svelte-chat-bot/src/Face.svelte
+++ svelte-chat-bot/src/Face.svelte
@@ -1,9 +1,11 @@
 <script>
-  export let size = 1;
+  export let size = 1,
+    index = 0;
+  const faces = [":)", ":(", ">:(", ":|"];
 </script>
 
 <style>
 
 </style>
 
-<div style="font-size: {size}em">:)</div>
+<div style="font-size: {size}em">{faces[index]}</div>
